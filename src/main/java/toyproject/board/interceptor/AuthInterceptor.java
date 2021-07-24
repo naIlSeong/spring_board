3886f0dbf4faf25cfd4e8ea4ad1848be18096bb4
nailseong <nnis9857@gmail.com>
2021-07-24 16:43:21 +0900

    인터셉터 수정

--- src/main/java/toyproject/board/interceptor/AuthInterceptor.java
+++ src/main/java/toyproject/board/interceptor/AuthInterceptor.java
@@ -1,7 +1,6 @@
 package toyproject.board.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
-import toyproject.board.domain.member.Member;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,21 +13,39 @@ public class AuthInterceptor implements HandlerInterceptor {
                              HttpServletResponse response,
                              Object handler) throws Exception {
 
-        HttpSession session = request.getSession();
-        Object member = session.getAttribute("member");
-        if (member == null) {
-
+        HttpSession session = request.getSession(false); // 이미 존재하는 세션을 가져옴
+        if (session == null) {
+            response.sendError(401, "세션아이디가 필요합니다.");
             return false;
         }
 
+        Object member = session.getAttribute("member");
+        if (member == null) {
+            response.sendError(401, "로그인이 필요합니다.");
+            return false;
+        }
+
+        String userAgentAttribute = session.getAttribute("userAgent").toString();
         String userAgent = request.getHeader("User-Agent");
 
+        if (userAgent == null || !userAgent.equals(userAgentAttribute)) {
+            response.sendError(401, "User-Agent 오류");
+            return false;
+        }
+
+        String ipAttribute = session.getAttribute("ip").toString();
         String ip = request.getHeader("X-FORWARDED-FOR");
         if (ip == null) {
             ip = request.getRemoteAddr();
         }
 
+        if (ip == null || !ip.equals(ipAttribute)) {
+            response.sendError(401, "ip 오류");
+            return false;
+        }
+
         return true;
+
     }
 
 }
