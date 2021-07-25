f0cff64874d5a7da01e847809ecc76ad1f204a3c
nailseong <nnis9857@gmail.com>
2021-07-25 18:47:18 +0900

    멤버 서비스 테스트 수정

--- src/test/java/toyproject/board/service/MemberServiceTest.java
+++ src/test/java/toyproject/board/service/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package toyproject.board.service;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,6 +34,7 @@ class MemberServiceTest {
      * 회원가입 테스트
      * memberService.join();
      */
+    @Tag("join")
     @Test
     void 회원가입_성공() throws Exception {
         // give
@@ -51,6 +53,7 @@ class MemberServiceTest {
         assertThat(result.getUsername()).isEqualTo("test");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_값이_빠짐_1() throws Exception {
         // give
@@ -68,6 +71,7 @@ class MemberServiceTest {
                 .withMessage("이름과 비밀번호는 필수 값입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_값이_빠짐_2() throws Exception {
         // give
@@ -84,6 +88,7 @@ class MemberServiceTest {
                 .withMessage("이름과 비밀번호는 필수 값입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_값이_빠짐_3() throws Exception {
         // give
@@ -100,6 +105,7 @@ class MemberServiceTest {
                 .withMessage("이름과 비밀번호는 필수 값입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_값이_빠짐_4() throws Exception {
         // give
@@ -117,6 +123,7 @@ class MemberServiceTest {
                 .withMessage("이름과 비밀번호는 필수 값입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_같은_이름의_회원이_존재_1() throws Exception {
         // give
@@ -139,6 +146,7 @@ class MemberServiceTest {
                 .withMessage("이미 존재하는 이름입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_같은_이름의_회원이_존재_2() throws Exception {
         // give
@@ -161,6 +169,7 @@ class MemberServiceTest {
                 .withMessage("이미 존재하는 이름입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_같은_이름의_회원이_존재_3() throws Exception {
         // give
@@ -183,6 +192,7 @@ class MemberServiceTest {
                 .withMessage("이미 존재하는 이름입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_이름_길이_1() throws Exception {
         // give
@@ -200,6 +210,7 @@ class MemberServiceTest {
                 .withMessage("이름의 길이는 4자 이상, 24자 이하입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_이름_길이_2() throws Exception {
         // give
@@ -217,6 +228,7 @@ class MemberServiceTest {
                 .withMessage("이름의 길이는 4자 이상, 24자 이하입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_이름_길이_3() throws Exception {
         // give
@@ -234,6 +246,7 @@ class MemberServiceTest {
                 .withMessage("이름의 길이는 4자 이상, 24자 이하입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_비밀번호_길이() throws Exception {
         // give
@@ -251,6 +264,7 @@ class MemberServiceTest {
                 .withMessage("비밀번호의 길이는 8자 이상입니다.");
     }
 
+    @Tag("join")
     @Test
     void 회원가입_암호화() throws Exception {
         // give
@@ -273,8 +287,9 @@ class MemberServiceTest {
      * 로그인 테스트
      * memberService.login()
      */
+    @Tag("login")
     @Test
-    void 로그인() throws Exception {
+    void 로그인_성공() throws Exception {
         // give
         MemberDto dto = MemberDto.builder()
                 .username("test")
@@ -298,10 +313,55 @@ class MemberServiceTest {
 
     }
 
+    @Tag("login")
+    @Test
+    void 로그인_실패_이름() throws Exception {
+        // give
+
+        // when
+        MemberDto loginDto = MemberDto.builder()
+                .username("test")
+                .password("12341234")
+                .build();
+
+        // memberService.login(loginDto);
+
+        // then
+        assertThatThrownBy(() -> memberService.login(loginDto))
+                .hasMessage("이름을 다시 확인해주세요.");
+
+    }
+
+    @Tag("login")
+    @Test
+    void 로그인_실패_비밀번호() throws Exception {
+        // give
+        MemberDto memberDto = MemberDto.builder()
+                .username("test")
+                .password("12341234")
+                .build();
+
+        memberService.join(memberDto);
+
+        // when
+        MemberDto loginDto = MemberDto.builder()
+                .username("test")
+                .password("43214321")
+                .build();
+
+        // memberService.login(loginDto);
+
+        // then
+        assertThatThrownBy(() -> memberService.login(loginDto))
+                .hasMessage("비밀번호를 다시 확인해주세요.");
+
+    }
+
     /**
      * 회원 탈퇴 테스트
      * memberService.withdrawal()
      */
+    @Tag("withdrawal")
     @Test
     void 탈퇴_성공() throws Exception {
         // give
@@ -321,6 +381,7 @@ class MemberServiceTest {
         assertThat(result).isTrue();
     }
 
+    @Tag("withdrawal")
     @Test
     void 탈퇴_예외() throws Exception {
         // give
@@ -334,10 +395,11 @@ class MemberServiceTest {
         em.clear();
 
         // when
-        boolean result = memberService.withdrawal(12121L);
+        // memberService.withdrawal(12121L);
 
         // then
-        assertThat(result).isFalse();
+        assertThatThrownBy(() -> memberService.withdrawal(12121L))
+                .hasMessage("유저를 찾을 수 없습니다.");
     }
 
 }
