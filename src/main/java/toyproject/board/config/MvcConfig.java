28fb825e0f0a34b753cee102c1274a5368b2f348
nailseong <nnis9857@gmail.com>
2021-07-27 16:00:57 +0900

    회원 검색 버그 수정

--- src/main/java/toyproject/board/config/MvcConfig.java
+++ src/main/java/toyproject/board/config/MvcConfig.java
@@ -1,9 +1,13 @@
 package toyproject.board.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import toyproject.board.interceptor.AuthInterceptor;
+
+import java.util.List;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurationSupport {
@@ -12,6 +16,11 @@ public class MvcConfig extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor())
                 .addPathPatterns("/member/withdrawal");
+    }
+
+    @Override
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
     }
 
 }
