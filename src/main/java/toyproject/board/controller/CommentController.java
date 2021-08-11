4f44e37008dc57bbe2a4b120a8334328ca574a58
nailseong <nnis9857@gmail.com>
2021-08-12 00:59:29 +0900

    댓글 수정 컨트롤러

--- src/main/java/toyproject/board/controller/CommentController.java
+++ src/main/java/toyproject/board/controller/CommentController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.*;
 import toyproject.board.domain.member.Member;
 import toyproject.board.dto.comment.CommentResponseDto;
 import toyproject.board.dto.comment.CreateCommentRequestDto;
+import toyproject.board.dto.comment.UpdateCommentRequestDto;
 import toyproject.board.service.CommentService;
 
 import javax.servlet.http.HttpSession;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RequiredArgsConstructor
 @RestController
@@ -39,6 +41,28 @@ public class CommentController {
         return CommentResponseDto.builder()
                 .httpStatus(CREATED)
                 .commentId(commentId)
+                .build();
+    }
+
+    @PostMapping("/update")
+    public CommentResponseDto updateComment(@RequestBody UpdateCommentRequestDto dto,
+                                            HttpSession session) {
+
+        Long commentId;
+        Member member = (Member) session.getAttribute("member");
+
+        if (member != null) {
+
+            dto.setMember(member);
+            commentId = commentService.updateComment(dto.toDto(member));
+
+        } else {
+            commentId = commentService.updateComment(dto.toDto());
+        }
+
+        return CommentResponseDto.builder()
+                .httpStatus(OK)
+                .commentId(dto.getCommentId())
                 .build();
     }
 
