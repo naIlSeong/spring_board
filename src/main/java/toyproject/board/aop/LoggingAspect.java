77932d99b7e6bc769d2b4eef93104598a0e0254c
nailseong <nnis9857@gmail.com>
2021-09-26 03:06:59 +0900

    AfterThrowing 로그 추가

--- src/main/java/toyproject/board/aop/LoggingAspect.java
+++ src/main/java/toyproject/board/aop/LoggingAspect.java
@@ -1,7 +1,9 @@
 package toyproject.board.aop;
 
 import com.google.common.base.Joiner;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
@@ -33,6 +35,10 @@ public class LoggingAspect {
     public void onRequest() {
     }
 
+    @Pointcut("within(toyproject.board.service..*)")
+    public void afterThrow() {
+    }
+
     @Around("toyproject.board.aop.LoggingAspect.onRequest()")
     public Object doLogging(ProceedingJoinPoint pjp) throws Throwable {
         HttpServletRequest request =
@@ -52,6 +58,34 @@ public class LoggingAspect {
             stopWatch.stop();
             logger.info("Request: {} {}{} | {} | {}ms", request.getMethod(), request.getRequestURI(),
                     params, request.getRemoteHost(), stopWatch.getTotalTimeMillis());
+        }
+    }
+
+    @AfterThrowing(pointcut = "toyproject.board.aop.LoggingAspect.afterThrow()", throwing = "exception")
+    public void doExceptionLogging(JoinPoint joinPoint, Throwable exception) {
+        if (exception.getMessage() == null) {
+
+            StringBuilder builder = new StringBuilder();
+            builder.append("Exception: ");
+
+            builder.append(exception.getClass().toString().split("class ")[1]);
+            builder.append(" | ");
+
+            builder.append(joinPoint.getSignature().toShortString());
+
+            Object[] args = joinPoint.getArgs();
+            for (int i = 0; i < args.length; i++) {
+                if (i == 0) {
+                    builder.append(" | Args -> ");
+                } else {
+                    builder.append(", ");
+                }
+
+                builder.append(args[i]);
+            }
+
+            logger.error(builder.toString());
+
         }
     }
 
